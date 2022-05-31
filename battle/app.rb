9f3330ede6719b9bba86be03bc2fd4d180387e9c
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-31 12:39:10 +0100

    redirectetd post request to get play

--- battle/app.rb
+++ battle/app.rb
@@ -6,13 +6,21 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_one_name = params[:player_one_name]
-    @player_two_name = params[:player_two_name]
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect "/play"
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     erb(:play)
   end
 
