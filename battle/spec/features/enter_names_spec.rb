75c0df5d7620245b35d402fb07794c1b0bc7688e
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-31 15:37:55 +0100

    can see player 2 score and stored log in in a method

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -1,10 +1,7 @@
 RSpec.feature "testing form completion and submission" do
   scenario "data is added to form and then displayed to the user" do
-    visit "/"
-    fill_in "player_one_name", with: "Sophie"
-    fill_in "player_two_name", with: "Rosie"
-    click_button "Submit"
+    sign_in_and_play
     # save_and_open_page
-    expect(page).to have_text("Sophie vs Rosie")
+    expect(page).to have_text("Sophie vs Aisha")
   end
 end
---
+++ battle/spec/features/hit_points_spec.rb
@@ -0,0 +1,7 @@
+feature "hitpoints" do
+  scenario "displays hit points" do
+    sign_in_and_play
+    # save_and_open_page
+    expect(page).to have_text("Aisha: 60HP")
+  end
+end
